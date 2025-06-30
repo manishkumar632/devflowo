44de25f68544af257c6727c218ed5e944feba8a6
Manish Kumar <manishmu632@gmail.com>
2025-06-30 21:28:06 +0530

    commit

--- frontend/devflowo/next.config.mjs
+++ frontend/devflowo/next.config.mjs
@@ -1,4 +1,12 @@
+// File: next.config.mjs
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/devflowo' : '',
+  assetPrefix: isProd ? '/devflowo/' : '',
+};
 
 export default nextConfig;
