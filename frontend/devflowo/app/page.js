47e3ce0916993ad155a19d2e450fdd4097084aec
Manish Kumar <manishmu632@gmail.com>
2025-07-01 09:44:02 +0530

    commit

--- frontend/devflowo/app/page.js
+++ frontend/devflowo/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div>
-      <p className="text-3xl">Hello World</p>
-    </div>
+      <div>
+        <p className="text-3xl">Hello World</p>
+      </div>
     </>
   );
 }
